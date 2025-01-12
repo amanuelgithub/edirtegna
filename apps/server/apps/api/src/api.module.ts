6eb9c1b757cd2954581e432fda720d1c75a75a17
Amanuel Girma <amanuelgirma070@gmail.com>
2025-01-12 22:04:06 +0300

    configured project to use TRPC, - tsconfig, install necessary packages, create a sample greeting message and served it form the server and utilized it in the expo application. :)

--- apps/server/apps/api/src/api.module.ts
+++ apps/server/apps/api/src/api.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 import { UsersModule } from '@app/users';
+import { TrpcModule } from '@app/trpc';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TrpcModule],
   controllers: [ApiController],
   providers: [ApiService],
 })
