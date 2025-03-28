6f51af0ebd2244dfdc57467beaa048ed87daa219
Amanuel Girma <amanuelgirma070@gmail.com>
2025-03-28 21:31:26 +0300

    chore: configured react-native-reanimated

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,6 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+      // @caution react-native-reanimated/plugin has to be listed last.
+      "react-native-reanimated/plugin",
+    ],
   };
 };
