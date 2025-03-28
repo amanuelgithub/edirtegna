9bf227afc642765b24ec065a14290ad5412ea767
Amanuel Girma <amanuelgirma070@gmail.com>
2025-03-28 22:37:22 +0300

    fix: issue with react-native-renatimated configuration error

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,11 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-      // @caution react-native-reanimated/plugin has to be listed last.
-      "react-native-reanimated/plugin",
-    ],
+    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
