f7d35ffe9e6f81e40ae9ecd3a9db57dcfb80ee6e
Amanuel Girma <amanuelgirma070@gmail.com>
2025-03-30 14:34:36 +0300

    chore: changed the role value

--- backend/libs/db/src/_seed/02-role.ts
+++ backend/libs/db/src/_seed/02-role.ts
@@ -5,13 +5,13 @@ import { EnumValues } from '@app/shared';
 export const COMPANY_ADMIN = 1;
 export const COMPANY_CUSTOMER_SERVICE = 2;
 // CUSTOMER realm
-export const PARTNER_ADMIN = 3;
-export const PARTNER_CUSTOMER_SERVICE = 4;
+export const CUSTOMER = 3;
 
 //
 //-------- HELPERS ----
 export const COMPANY_ROLE = [COMPANY_ADMIN, COMPANY_CUSTOMER_SERVICE] as const;
-export const CUSTOMER_ROLE = [PARTNER_ADMIN, PARTNER_CUSTOMER_SERVICE] as const;
+// export const CUSTOMER_ROLE = [PARTNER_ADMIN, PARTNER_CUSTOMER_SERVICE] as const;
+export const CUSTOMER_ROLE = [CUSTOMER] as const;
 export const ROLE = [...COMPANY_ROLE, ...CUSTOMER_ROLE] as const;
 
 //
@@ -20,9 +20,8 @@ export const ROLES_SEED = [
   // administrative
   { id: COMPANY_ADMIN, name: 'COMPANY_ADMIN', description: 'Company Admin', realm: 'ADMIN' },
   { id: COMPANY_CUSTOMER_SERVICE, name: 'COMPANY_CUSTOMER_SERVICE', description: 'Company CS', realm: 'ADMIN' },
-  // customer
-  { id: PARTNER_ADMIN, name: 'PARTNER_ADMIN', description: 'Partner Admin', realm: 'CUSTOMER' },
-  { id: PARTNER_CUSTOMER_SERVICE, name: 'PARTNER_CUSTOMER_SERVICE', description: 'Partner CS', realm: 'CUSTOMER' },
+  // // customer
+  { id: CUSTOMER, name: 'CUSTOMER', description: 'Customer', realm: 'CUSTOMER' },
 ];
 
 export type Role = EnumValues<typeof ROLE>;
